ff7a53bc3f7dc77fb3f7caf39884d9f137adb170
Tommy Hosewol <t.hosewol@gmail.com>
2020-12-16 12:43:04 +0100

    Add Repository to GearsController

--- NinjaManager.Web/Controllers/GearsController.cs
+++ NinjaManager.Web/Controllers/GearsController.cs
@@ -1,9 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NinjaManager.Data.Repositories;
 
 namespace NinjaManager.Web.Controllers
 {
     public class GearsController : Controller
     {
+        private readonly GearsRepository _gearsRepository;
+
+        public GearsController(GearsRepository gearsRepository)
+        {
+            _gearsRepository = gearsRepository;
+        }
+
         // GET
         public IActionResult Index()
         {
