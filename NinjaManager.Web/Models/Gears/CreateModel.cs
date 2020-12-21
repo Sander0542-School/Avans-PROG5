313ece227b9a2beddef7ff84eea10856dfc797e8
Tommy Hosewol <t.hosewol@gmail.com>
2020-12-21 18:12:59 +0100

    Cleanup

--- NinjaManager.Web/Models/Gears/CreateModel.cs
+++ NinjaManager.Web/Models/Gears/CreateModel.cs
@@ -1,16 +1,28 @@
 ﻿using NinjaManager.Data.Models;
+using System;
 using System.ComponentModel.DataAnnotations;
 
 namespace NinjaManager.Web.Models.Gears
 {
-    public class CreateModel
+    public class CreateModel 
     {
+        [Required]
         public string Name { get; set; }
+
+        [Range(minimum: 0, maximum: Int32.MaxValue)]
+        [Required]
         public int Gold { get; set; }
+
+        [Required]
         public int Strength { get; set; }
+
+        [Required]
         public int Intelligence { get; set; }
+
+        [Required]
         public int Agility { get; set; }
 
+        [Required]
         public Gear.GearCategory Category { get; set; }
     }
 }
--- NinjaManager.Web/Models/Gears/Model.cs
+++ NinjaManager.Web/Models/Gears/Model.cs
@@ -1,6 +1,7 @@
 ﻿using NinjaManager.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,11 +10,17 @@ namespace NinjaManager.Web.Models.Gears
     public class Model
     {
         public int Id { get; set; }
+
         public string Name { get; set; }
+
         public int Gold { get; set; }
+
         public int Strength { get; set; }
+
         public int Intelligence { get; set; }
+
         public int Agility { get; set; }
+
         public Gear.GearCategory Category { get; set; }
     }
 }
